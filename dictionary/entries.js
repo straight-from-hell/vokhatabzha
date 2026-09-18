15c69a960c2220c1098846c82b6b75855b2179dc
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-17 21:52:03 -0400

    retrying the sorting loop

--- dictionary/entries.js
+++ dictionary/entries.js
@@ -5,6 +5,7 @@ fetch("../words.json")
   for (var i = 0; i < 35; i++){ // repeat for every first letter
     var table = document.getElementById(String(i));
     var section = [];
+    var longest = 0;
 
     sorted.forEach(entry => { // creates a section w only words that start w the same letter
       if (entry.numVal[0] == i){
@@ -12,39 +13,46 @@ fetch("../words.json")
       }
     });
 
-    section = section.sort((a,b) => a.numVal[1] - b.numVal[1]); // sort section by second letter
-    // var wordList = [];
-    // section.forEach(entry => {
-    //   wordList.push(entry.numVal); // get only the arrays of numerical values
-    //   console.log(wordList);
-    // });
-    // var longest = wordList.reduce((a, b) => a.length > b.length ? a : b); // find the longest word
-    // var collected = [];
-    // var smallSec = [];
+    section.forEach(entry => { // determines the longest word in the section
+      if (entry.numVal.length > longest){
+        longest = entry.numVal.length;
+      }
+    });
 
-    // for (var x = 1; x < longest.length; x++){ // repeat until there's no letters left
-    //   smallSec = []; // empties the tracker
-    //   for (var j = 0; j < 35; j++){
-    //     section.forEach(entry => { 
-    //       if (entry.numVal[x] == j){
-    //         smallSec.push(entry); // get a list of only words that have the same next letter (x)
-    //       }
-    //     });
-    //     smallSec = smallSec.sort((a,b) => a.numVal[x+1] - b.numVal[x+1]); // sort by same letter after (x+1)
-    //   }
-    //   smallSec.forEach(entry => { // appends the words to the beginning
-    //     collected.push(entry);
-    //   });
-    // }
-    // section = collected;
+    // section = section.sort((a,b) => a.numVal[1] - b.numVal[1]); // sort section by second letter
+
+    // loop to sort each section alphabetically
+    // step 1: sort the larget unsorted section
+    // step 2: collect all the words that have a matching letter in the same spot
+    var base = section;
+    base = base.sort((a,b) => a.numVal[1] - b.numVal[1]);
+    var group = [];
+    var storage = [];
+
+    for (var j = 2; j <= longest; j++){
+      for (var k = 0; k < 35; k++){ // for each numVal index, each letter has to be checked for duplicates
+        base.forEach(entry => { 
+          if (entry.numVal[j] == k){
+            group.push(entry);
+          }
+        });
+
+        group.forEach(word => { // add the group to storage
+          storage.push(word);
+        });
+        group = []; // empty the group to reuse next iteration
+      }
+
+      base = storage;
+    }
+
 
     // making arrays to use in the function that makes the tables
     var wordList = [];
     var keyList = [];
     var descList = [];
-    section.forEach(entry => {
+    base.forEach(entry => {
       wordList.push(entry.entry);
-      // console.log(entry.entry);
       keyList.push(entry.speechPart.key);
       descList.push(entry.desc);
     });
@@ -57,7 +65,6 @@ function makeTable(arrE, arrK, arrD, place){
   var lineEntry = 0;
   var lineInfo = 0;
   var text = "<table>";
-  // place.innerHTML += "<table>";
 
   while (lineInfo < arrE.length){
     text += "<tr class='entry'>";
@@ -66,7 +73,6 @@ function makeTable(arrE, arrK, arrD, place){
         break;
       }
       text += "<td>"+arrE[i+lineEntry]+"</td>";
-      // console.log(arrE[lineEntry]);
     }
     lineEntry+= 3;
     
