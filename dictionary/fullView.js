879ac83c47f8e600e4bb500c3939c6d3a30c965e
straight-from-hell <123125398+straight-from-hell@users.noreply.github.com>
2026-09-25 10:31:13 -0400

    Update fullView.js

--- dictionary/fullView.js
+++ dictionary/fullView.js
@@ -3,3 +3,17 @@ var entry = params.get("word");
 // console.log(entry);
 var heading = document.getElementById("entry");
 heading.innerHTML = entry;
+
+var type = document.getElementById("speechPart");
+var desc = document.getElementById("desc");
+
+fetch("../words.json")
+.then(data => data.json())
+.then(json => {
+  json.forEach(word => {
+    if (word.entry == entry){
+      type.innerHTML = word.speechPart.term;
+      desc.innerHTML = word.desc;
+    }
+  });
+});
